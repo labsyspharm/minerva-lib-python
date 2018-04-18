51f74013c9de62883e6318c6c487048d354d143e
thejohnhoffer <hoffexi@gmail.com>
2018-04-18 13:08:51 -0400

    add to_f32 test

--- tests/blend.py
+++ tests/blend.py
@@ -2,6 +2,7 @@
 
 import pytest
 import numpy as np
+from minerva_lib.blend import to_f32
 from minerva_lib.blend import linear_bgr
 
 
@@ -185,3 +186,26 @@ def test_channel_color_mismatch(image_2channel, range_all, color_white):
         linear_bgr(image_2channel,
                    colors=[color_white],
                    ranges=[range_all, range_all])
+
+
+def test_to_f32_full():
+    ''' Test f32 conversion across full range
+    '''
+    # Sample range from 0 to 65535
+    img_in = np.uint16([
+        [0],
+        [256],
+        [65535],
+    ])
+
+    # The same range from 0 to 1
+    img_ok = np.float32([
+        [0.0],
+        [256.0 / 65536.0],
+        [65535.0 / 65536.0],
+    ])
+
+    # Check to_f32 handles uint16 range
+    img_out = to_f32(img_in)
+    img_pair = img_ok, img_out
+    Key.full_check(img_pair)
